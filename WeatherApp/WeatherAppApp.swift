a42140fe6b4499fe080ad005268eb19cb5906ab5
Dulneth Bernard <dulneth.bernard@gmail.com>
2025-12-14 17:27:12 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Dulneth Bernard on 14/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
